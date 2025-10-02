e625fdba74172e8f847425241a2b52b346cec19d
BraxtonBrown <10944268@uvu.edu>
2025-10-02 17:53:42 -0600

    Revert "update checks and add joint tracking"
    This reverts commit 683c01eaf4125b5c196bb47826c8ad6aa78224d7.

--- Unity Project/The Shadow Project/Assets/Scripts/Python_Data/Body_Tracking.cs
+++ Unity Project/The Shadow Project/Assets/Scripts/Python_Data/Body_Tracking.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
-using System.Collections.Generic;
+
 public class Body_Tracking : MonoBehaviour
 {
     public UDP udpBody;
+    public GameObject bodyPoint; // Single GameObject to move
     [Tooltip("in case not ports are assigned it will use these th make point and carte a skeleton for testing")]
     public GameObject bodyPointPrefab; // Prefab to clone for each landmark
     [Tooltip("Assign point in order of Left arm 1-3 , right arm 1-3, left leg 1-3, right leg 1-3, then last is mid point")]
@@ -13,26 +14,30 @@ public class Body_Tracking : MonoBehaviour
     [Tooltip("deaflut is (X = -7, Y = 12, Z = 0)")]
     public Vector3 offset = new Vector3(0, 0, 0);
 
-    private void FixedUpdate()
+    private void Update()
     {
         string data = udpBody.data;
-
-        if (string.IsNullOrEmpty(data))
-            return;
-
+        
         data = data.Trim(new char[] { '[', ']' });
         string[] points = data.Split(',');
 
-        int pointNumber = points.Length / 2; // assuming data is x,y,z
-
-        // Create missing points
-        while (bodyPoints.Count < pointNumber)
+        // Ensure there are enough points
+        if (points.Length < 2)
         {
+            Debug.LogError("Not enough points received.");
+            return;
             GameObject newPoint = Instantiate(bodyPointPrefab);
             // newPoint.transform.parent = transform; // Keep hierarchy clean
             bodyPoints.Add(newPoint);
         }
 
+        // Parse the first landmark position
+        // x-xis is minused by 7 because it offenses the position of the body by 7 when transferred
+        float x = 7 - float.Parse(points[0]) / 100;
+        float y = float.Parse(points[1]) / 100;
+        
+        // Update the position of the single GameObject
+        bodyPoint.transform.localPosition = new Vector3(x, y, bodyPoint.transform.localPosition.z);
         // Update positions
         for (int i = 0; i < pointNumber; i++)
         {
