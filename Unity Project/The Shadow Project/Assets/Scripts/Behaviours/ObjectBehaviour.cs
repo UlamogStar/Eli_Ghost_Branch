ebb364c9ce3698040dcf704335f2039234636976
Zackery Elmer <Zackeryelmer@comcast.net>
2025-11-03 14:26:17 -0700

    Added Object Return Script, and HealthValue System

--- Unity Project/The Shadow Project/Assets/Scripts/Behaviours/ObjectBehaviour.cs
+++ Unity Project/The Shadow Project/Assets/Scripts/Behaviours/ObjectBehaviour.cs
@@ -1,8 +1,8 @@
 /*
 Originial Coder: Owynn A.
 Recent Coder: Zackery E.
-Recent Changes: Added variables and headers
-Last date worked on: 10/28/2025
+Recent Changes: Added Damaging Behvaior
+Last date worked on: 11/3/2025
 */
 
 using UnityEngine;
@@ -11,13 +11,28 @@ public class ObjectBehaviour : MonoBehaviour
 {
     [Header("Settings")]
     public ObjectSize size = ObjectSize.Small;
-    public IntData damage;
+    public int damage = 10;
     public float throwSpeed = 1f;
-    public bool returnable = false, thrown = false;
+    public bool returnable = false, thrown = false, returned = false, hit = false;
 
     [Header("Info")]
     public ObjectSpawner originSpawn;
 
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out HealthValueBehavior healthValue) && hit == false)
+        {
+            if (other.CompareTag("Ghost") && returned == true)
+            {
+                healthValue.Damage(damage);
+                hit = true;
+            } else if (other.CompareTag("Player"))
+            {
+                healthValue.Damage(damage);
+                hit = true;
+            }
+        }
+    }
 }
 
 // Object sizes must be from small -> large
