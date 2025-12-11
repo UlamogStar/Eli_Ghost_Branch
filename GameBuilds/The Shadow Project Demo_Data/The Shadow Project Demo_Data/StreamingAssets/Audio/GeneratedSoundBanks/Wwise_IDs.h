7dcf0271060545664756f11bb15ff3863eb3ffb5
Natalie <10840498@uvu.edu>
2025-12-11 01:14:40 -0700

    Game build ready!!

---
+++ GameBuilds/The Shadow Project Demo_Data/The Shadow Project Demo_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,95 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMB_ATTIC_START = 1697873554U;
+        static const AkUniqueID GHOST_ATTACK = 979070061U;
+        static const AkUniqueID OBJECT_HIT_PLAYER = 2405462070U;
+        static const AkUniqueID TEST_EVENT = 4258653052U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace ITEM_SIZE
+        {
+            static const AkUniqueID GROUP = 1566734580U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID LARGE = 4284352190U;
+                static const AkUniqueID MEDIUM = 2849147824U;
+                static const AkUniqueID SMALL = 1846755610U;
+            } // namespace SWITCH
+        } // namespace ITEM_SIZE
+
+        namespace ITEM_TYPE
+        {
+            static const AkUniqueID GROUP = 1281648863U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CANDELABRA = 3353308030U;
+                static const AkUniqueID CANDLE = 2912468606U;
+                static const AkUniqueID CARDBOARD = 1229756253U;
+                static const AkUniqueID DEFAULT = 782826392U;
+                static const AkUniqueID STOOL = 1200188298U;
+                static const AkUniqueID TABLE = 2048817515U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace ITEM_TYPE
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID GHOST_HEALTH = 180621091U;
+        static const AkUniqueID PLAYER_HEALTH = 215992295U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+        static const AkUniqueID TEST = 3157003241U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID ENEMIES = 2242381963U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID NON_WORLD = 838047381U;
+        static const AkUniqueID OBJECTS = 1695690031U;
+        static const AkUniqueID PLAYER = 1069431850U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID UI = 1551306167U;
+        static const AkUniqueID WORLD = 2609808943U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID ENV_FX = 1802038219U;
+        static const AkUniqueID OBJECTS_FX = 591668312U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
