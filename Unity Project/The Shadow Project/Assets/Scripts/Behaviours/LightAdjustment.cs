b6a66f201464cd3bf4572bfe1af578de90806c30
Natalie <10840498@uvu.edu>
2025-12-10 23:40:48 -0700

    Quick Reset and Light  Adjustment Buttons

---
+++ Unity Project/The Shadow Project/Assets/Scripts/Behaviours/LightAdjustment.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class LightAdjustment : MonoBehaviour
+{
+    private float light;
+    private Light adjustLight;
+    void Start()
+    {
+        light = this.GetComponent<Light>().intensity;
+        adjustLight = this.GetComponent<Light>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Equals))
+        {
+            light += 10;
+            adjustLight.intensity = light;
+        }
+        if (Input.GetKeyDown(KeyCode.Minus))
+        {
+            light -= 10;
+            adjustLight.intensity = light;
+        }
+    }
+}
