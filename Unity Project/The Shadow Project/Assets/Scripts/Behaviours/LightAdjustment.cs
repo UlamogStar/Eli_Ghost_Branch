50f898ea497c61c07ae83d7300e686a3172d2498
Elijah <vigorilious@gmail.com>
2026-01-26 16:17:16 -0700

    Debug/System Menu set up
    Canvas is toggleable with Tab and can be changed via the menu panel in said canvas. I made one new script called MenuController which controls the menu toggle as well as changes to two scripts to make the functions public/visible to the buttons.

--- Unity Project/The Shadow Project/Assets/Scripts/Behaviours/LightAdjustment.cs
+++ Unity Project/The Shadow Project/Assets/Scripts/Behaviours/LightAdjustment.cs
@@ -2,24 +2,49 @@ using UnityEngine;
 
 public class LightAdjustment : MonoBehaviour
 {
-    private float light;
+    private float intensityValue;
     private Light adjustLight;
+
     void Awake()
     {
-        light = this.GetComponent<Light>().intensity;
-        adjustLight = this.GetComponent<Light>();
+        adjustLight = GetComponent<Light>();
+        if (adjustLight != null) intensityValue = adjustLight.intensity;
     }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Equals))
         {
-            light += 10;
-            adjustLight.intensity = light;
+            ChangeIntensity(10f);
         }
         if (Input.GetKeyDown(KeyCode.Minus))
         {
-            light -= 10;
-            adjustLight.intensity = light;
+            ChangeIntensity(-10f);
         }
+    }
+
+    //Added by Elijah: Public methods for UI Buttons
+    public void IncreaseIntensity()
+    {
+        ChangeIntensity(10f);
+    }
+
+    public void DecreaseIntensity()
+    {
+        ChangeIntensity(-10f);
+    }
+
+    private void SetIntensity(float value)
+    {
+        intensityValue = value;
+        if (adjustLight == null) adjustLight = GetComponent<Light>();
+        if (adjustLight != null) adjustLight.intensity = intensityValue;
+    }
+
+    private void ChangeIntensity(float delta)
+    {
+        intensityValue += delta;
+        if (adjustLight == null) adjustLight = GetComponent<Light>();
+        if (adjustLight != null) adjustLight.intensity = intensityValue;
     }
 }
