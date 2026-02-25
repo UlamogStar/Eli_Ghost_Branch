2fb7fd04a93e6da6f717a14a9e3d4cfb0ceb6f0f
Owynn Armstrong <10932343@uvu.edu>
2026-02-24 21:04:22 -0700

    Set up player healthbar visibility and ghost throwing

--- Unity Project/The Shadow Project/Assets/Scripts/Behaviours/LoadPlayer.cs
+++ Unity Project/The Shadow Project/Assets/Scripts/Behaviours/LoadPlayer.cs
@@ -1,22 +1,36 @@
 using UnityEngine;
 using UnityEngine.Events;
-
+using System.Collections;
+using System.Collections.Generic;
 public class LoadPlayer : MonoBehaviour
 {
     public UnityEvent OnLoadPlayer, OnUnloadPlayer;
+    public bool wait = false;
+
+    public void OnTriggerExit(Collider other)
+    {
+        StartCoroutine(RemovePlayer(other));
+    }
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("JoinTrigger"))
         {
             OnLoadPlayer.Invoke();
+            wait = true;
         }
+        
     }
 
-    public void OnTriggerExit(Collider other)
+    public IEnumerator RemovePlayer(Collider other)
     {
-        if (other.CompareTag("JoinTrigger"))
+        wait = false;
+        yield return new WaitForSeconds(1f);
+        if (wait == false)
         {
-            OnUnloadPlayer.Invoke();
+            if (other.CompareTag("JoinTrigger"))
+            {
+                OnUnloadPlayer.Invoke();
+            }
         }
     }
 }
